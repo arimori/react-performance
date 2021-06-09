1581052469779d649c95cb4ec973a4e2586712af
NaoshiAC <naoshiarimori@gmail.com>
2021-06-09 18:42:16 -0300

    Add memo on ProductItem component

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 interface ProcustItemProps {
   product: {
     id: number;
@@ -6,10 +8,27 @@ interface ProcustItemProps {
   }
 }
 
-export function ProductItem({ product }: ProcustItemProps) {
+function ProductItemComponent({ product }: ProcustItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+// shallow compare -> comparação rasa
+// js: {} === {} => false
+// igualdade referencial
+
+/** When should use memo?
+ * 1. Pure Functional Components: only visual components (same props and same return)
+ * 2. Renders too often
+ * 3. Re-renders with same props 
+ * 4. Components medium to big size
+ * */
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  //este componente é igual?
+  //retornar condição que satisfaz uma nova renderização
+  return Object.is(prevProps.product, nextProps.product);
+});
